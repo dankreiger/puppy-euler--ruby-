5377755f0a697be617893c26df93b3d5ed9ce152
Dan Kreiger <dan@dankreiger.com>
2016-08-06 16:46:07 +0200

    Refactor problem 1

--- pup_euler1.rb
+++ pup_euler1.rb
@@ -1,17 +1,5 @@
 def pup_euler1
-a=*(1...1000)
-
-y = a.delete_if{ |x| x % 5 !=0 && x % 3 != 0 }
-
-  total = 0
-  i = 0
-   while i < 466
-    y.each{ |puppy| total = total + puppy }
-    return total
-    
-   i += 1
-   end
-   
+(1...1000).to_a.delete_if{ |x| x % 5 !=0 && x % 3 != 0 }.inject(:+)   
 end
 
 puts "\t\nThe puppy answer is: #{pup_euler1}.\n\n"
