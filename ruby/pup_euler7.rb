0c4ebd168b3088a8beebe88a0765324f7984c2e5
Dan Kreiger <dan@dankreiger.com>
2016-10-18 16:41:16 +0200

    Require prime for travis build

--- ruby/pup_euler7.rb
+++ ruby/pup_euler7.rb
@@ -1,8 +1,10 @@
+require 'prime'
+
 def pup_euler7
   x = Prime.take(10001)[-1]
 end
 
 unless ENV['RACK_ENV'] == 'test'
-  require_relative "vars";s=Vars::Sentence.new;require 'prime'
+  require_relative "vars";s=Vars::Sentence.new
   puts s.solution(pup_euler7)
 end
