07ac5437e95ae4e85b7b87f104550b3135c90aff
Dan Kreiger <dan@dankreiger.com>
2017-01-12 15:59:45 +0100

    puppydate spec

--- spec/pup_euler_spec.rb
+++ spec/pup_euler_spec.rb
@@ -1,19 +1,18 @@
+require 'spec_helper'
+require 'benchmark'
+
 describe PupEuler do
-  before do
-    @sniff = PupEuler.new
-  end
+  let(:sniff)  { PupEuler.new }
+
   # 1.upto(564) do |n|
   1.upto(13) do |n|
     context "pup_euler#{n}" do
-      solutions[n] ? it {expect(@sniff.puppy_method(n)).to eq(solutions[n])} : pending(" ")
+      solutions[n] ? it {expect(sniff.puppy_method(n)).to eq(solutions[n])} : pending(" ")
 
-      it "does not exceed 50ms runtime" do
-        start_time = Time.now
-        @sniff.puppy_method(n)
-        pup_bench = Time.now - start_time
+      it "does not exceed 40ms runtime" do
+        pup_bench = Benchmark.realtime { sniff.puppy_method(n) }
         puts "\tpup_bench: #{pup_bench}"
-        expect(pup_bench).to be <= 0.05
-
+        expect(pup_bench).to be <= 0.04
       end
     end
   end
